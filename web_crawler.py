8b7811a498d3b3574ac6e5a3ca89b07082b4f0b9
gitduk <wdkany@gmail.com>
2021-01-07 01:23:49 +0800

    update ParamFactory

--- web_crawler.py
+++ web_crawler.py
@@ -9,28 +9,32 @@ logger = logging.getLogger(__name__)
 
 class ParamFactory(object):
     """
-    Process requests params and cookies
+    Process requests params
     """
 
-    def __init__(self, url, body_str=None, header_str=None, cookie_str=None, overwrite=True):
+    def __init__(self, url, body=None, headers=None, cookies=None, overwrite=True):
         self._url = url
-        self._body_str = '' if body_str is None else body_str
-        self._param = ''
-        self._param_dict = {}
+        self._path = self._url.split('?')[0]
+        self._param_str = self._url.split('?')[-1]
+        self._body_str = '' if body is None else body
+        self._header_str = '' if headers is None else headers
+        self._cookie_str = '' if cookies is None else cookies
 
-        self._header_str = '' if header_str is None else header_str
-        self._cookie_str = '' if cookie_str is None else cookie_str
+        self._param_dict = {}
+        self._body_dict = {}
         self._header_dict = {}
         self._cookie_dict = {}
         self._cookie_jar = requests.cookies.RequestsCookieJar()
 
-        self.method = 'POST' if body_str else 'GET'
+        self.method = 'POST' if body else 'GET'
         self.post_type = 'form' if self.method == 'POST' else ''
-        self.path = self._url.split('?')[0]
 
         self.overwrite = overwrite
-        self.update_status = 0
-        self._init_all()
+
+        if self._param_str: self.str_to_dict(self._param_str, tag='param')
+        if self._body_str: self.str_to_dict(self._body_str, tag='body')
+        if self._header_str: self.str_to_dict(self._header_str, tag='header')
+        if self._cookie_str: self.str_to_dict(self._cookie_str, tag='cookie')
 
     def clear(self, key=None):
         if key == 'param':
@@ -47,46 +51,28 @@ class ParamFactory(object):
             self._cookie_dict.clear()
             self._cookie_jar.clear()
 
-    def _init_all(self):
-        # init param
-        if self.method == 'POST':
-            self.body_to_dict()
-        else:
-            self.url_param_to_dict()
-
-        # init header
-        if self._header_str != '':
-            self.trans_to_dict(to='header')
-
-        # init cookie
-        if self._cookie_str != '':
-            self.trans_to_dict(to='cookie')
-
     @property
     def url(self):
-        if self.method == 'GET' and '?' in self._url:
-            return self.path + '?' + '&'.join([f'{key}={value}' for key, value in self._param_dict.items()])
-        else:
-            return self._url
+        self._url = self._path + self.param
+        return self._url
 
     @property
     def param(self):
-        """
-        param for request
-        """
-        if self.method == 'POST' and self.post_type == 'form':
-            return '&'.join([f'{key}={value}' for key, value in self._param_dict.items()])
-        elif self.method == 'POST' and self.post_type == 'payload':
-            return json.dumps(self._param_dict)
-        else:
-            return '?' + '&'.join([f'{key}={value}' for key, value in self._param_dict.items()])
+        return '?' + '&'.join([f'{key}={value}' for key, value in self._param_dict.items()])
 
     @property
-    def header(self):
+    def body(self):
+        if self.post_type == 'form':
+            return '&'.join([f'{key}={value}' for key, value in self._body_dict.items()])
+        elif self.post_type == 'payload':
+            return json.dumps(self._body_dict)
+
+    @property
+    def headers(self):
         return self._header_dict
 
     @property
-    def cookie(self):
+    def cookies(self):
         return self._cookie_dict
 
     @property
@@ -98,6 +84,10 @@ class ParamFactory(object):
         return self._cookie_jar
 
     def cookie_setter(self, string):
+        """ build cookie dict use set-cookie field
+
+        string: set-cookie string
+        """
         string = re.search('(.*?); Path', string)
         if string:
             string = string.group(1)
@@ -110,94 +100,85 @@ class ParamFactory(object):
         else:
             print(f'Set-Cookie filed is None')
 
-    def update(self, key, value, tag='param'):
+    def _update(self, key, value, tag=None):
         if tag == 'param':
             self._param_dict[key] = value
+        elif tag == 'body':
+            self._body_dict[key] = value
         elif tag == 'header':
             self._header_dict[key] = value
         elif tag == 'cookie':
             self._cookie_dict[key] = value
 
-    def update_from_list(self, key_list, value_list, tag='param'):
-        for key, value in zip(key_list, value_list):
-            if tag == 'param':
-                self._param_dict[key] = value
-            elif tag == 'header':
-                self._header_dict[key] = value
-            elif tag == 'cookie':
-                self._cookie_dict[key] = value
+    def update(self, *args, tag='body'):
+        """ update tag use args
 
-    def update_from_dict(self, update_map, target_map=None, tag='param'):
+        tag: param, body, header, cookie
+        args: (dict,) or (str,str) or (list,list) or (dict,dict) or (list,list,dict)
+        """
+        if len(args) == 1 and isinstance(args[0], dict):
+            for key, value in args[0].items():
+                self._update(key, value, tag)
 
-        for key, value in update_map.items():
-            if target_map is None:
-                real_value = value
-            else:
-                real_value = target_map.get(value)
+        elif len(args) == 2 and isinstance(args[0], str):
+            key, value = args
+            self._update(key, value, tag)
 
-            if tag == 'param':
-                self._param_dict[key] = real_value if real_value else ''
-            elif tag == 'header':
-                self._header_dict[key] = real_value if real_value else ''
-            elif tag == 'cookie':
-                self._cookie_dict[key] = real_value if real_value else ''
+        elif len(args) == 2 and isinstance(args[0], list):
+            for key, value in zip(args):
+                self._update(key, value, tag)
 
-    def update_from_cookies(self, update_map):
-        for key, value in update_map.items():
-            self._param_dict[key] = self._cookie_dict.get(value)
+        elif len(args) == 2 and isinstance(args[0], dict):
+            for key, value in args[0].items():
+                value = args[1].get(value)
+                self._update(key, value, tag)
+        elif len(args) == 3 and isinstance(args[0], dict) and isinstance(args[2], dict):
+            for key, value in zip(args[0], args[1]):
+                value = args[2].get(value)
+                self._update(key, value, tag)
 
-    def body_to_dict(self, body=None):
-        body = body if body else self._body_str
-        if '=' in body and '&' in body:
-            self._param_dict = dict([_.split('=', 1) for _ in body.split('&')])
-        elif '=' in body:
-            self._param_dict = dict([body.split('=')])
-        elif ':' in body:
-            self.post_type = 'payload'
-            self._param_dict = json.loads(body)
+    def str_to_dict(self, string, tag=None):
+        """ translate string to dict
 
-    def url_param_to_dict(self, url=None):
-        url = url if url else self._url
-        if '?' in url:
-            self._param_dict = dict([_.split('=', 1) for _ in url.split('?')[-1].split('&')])
-        # else:
-        #     raise Exception('GET method with no "?"')
+        string: url, body, header or cookie
+        tag: param, body, header or cookie
+        rtype: dict
+        """
 
-    def trans_to_dict(self, string=None, to=None):
+        string = string.strip()
 
-        if string is None:
-            string = self._header_str.strip() if to == 'header' else self._cookie_str.strip()
+        if tag == 'param':
+            if '?' in string and '=' in string:
+                self._param_dict = dict([_.split('=', 1) for _ in string.split('?')[-1].split('&')])
 
-        if to == 'cookie':
-            split_params = [';', '=']
-        elif to == 'header':
-            split_params = ['\n', ':']
-        elif ';' in string and '=' in string and ':' in string:
-            split_params = ['\n', ':']
-        else:
-            split_params = [';', '=']
+        if tag == 'body':
+            if '=' in string and '&' in string:
+                self._body_dict = dict([_.split('=', 1) for _ in string.split('&')])
+            elif '=' in string:
+                self._body_dict = dict([string.split('=')])
+            elif ':' in string:
+                self.post_type = 'payload'
+                self._body_dict = json.loads(string)
 
-        target_dict = {}
-        for field in string.split(split_params[0]):
-            keys = target_dict.keys()
+        if tag == 'header' or tag == 'cookie':
+            split_params = ['\n', ':'] if tag == 'header' else [';', '=']
+            target_dict = {}
+            for field in string.split(split_params[0]):
+                keys = target_dict.keys()
 
-            key, value = field.split(split_params[1], 1)
-            key, value = [key.strip(), value.strip()]
+                key, value = field.split(split_params[1], 1)
+                key, value = [key.strip(), value.strip()]
 
-            value_in_dict = target_dict.get(key)
-            if key in keys and isinstance(value_in_dict, str):
-                target_dict[key] = [value_in_dict, value]
-            elif isinstance(value_in_dict, list):
-                target_dict[key].append(value)
-            else:
-                target_dict[key] = value
+                value_in_dict = target_dict.get(key)
+                if key in keys and isinstance(value_in_dict, str):
+                    target_dict[key] = [value_in_dict, value]
+                elif isinstance(value_in_dict, list):
+                    target_dict[key].append(value)
+                else:
+                    target_dict[key] = value
 
-        if to == 'header':
-            self._header_dict = target_dict
-        elif to == 'cookie':
-            self._cookie_dict = target_dict
-
-        return target_dict
+            if tag == 'header': self._header_dict = target_dict
+            if tag == 'cookie': self._cookie_dict = target_dict
 
     def __repr__(self):
-        return "url: {}\nparam: {}\nheaders: {}\ncookies: {}\n".format(self.url, self.param, self.header, self.cookie)
+        return "url: {}\nparam: {}\nheaders: {}\ncookies: {}\n".format(self.url, self.body, self.headers, self.cookies)
