5b62a1969d8862fab0efc11a465251ff391a3259
gitduk <wdkany@gmail.com>
2021-01-23 16:43:03 +0800

    update version to 0.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/gitduk/utils'
 EMAIL = 'wdkany@gmail.com'
 AUTHOR = 'kaige'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
