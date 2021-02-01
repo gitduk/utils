26aacbec9919ace2cdee3b38c815b8e08d76bd32
gitduk <wdkany@gmail.com>
2021-02-01 20:36:47 +0800

    add css extractor to Spider

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/gitduk/utils'
 EMAIL = 'wdkany@gmail.com'
 AUTHOR = 'kaige'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.5.1'
+VERSION = '0.5.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
