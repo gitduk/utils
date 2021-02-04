eccf74b5ccb0f7783f9a9ed9751fe4d66475635f
gitduk <wdkany@gmail.com>
2021-02-04 12:05:18 +0800

    remove index attribute form PriorityQueue

--- Dtautils/tools.py
+++ Dtautils/tools.py
@@ -4,11 +4,9 @@ import heapq
 class PriorityQueue:
     def __init__(self):
         self._queue = []
-        self._index = 0
 
     def push(self, item, priority):
-        heapq.heappush(self._queue, (priority, self._index, item))
-        self._index += 1
+        heapq.heappush(self._queue, (priority, item))
 
     def pop(self):
         return heapq.heappop(self._queue)[-1]
